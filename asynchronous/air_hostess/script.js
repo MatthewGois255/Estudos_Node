38a015dd774b2b4b98828fca1a5d9bd0db507d50
MatthewGois255 <matheusgois.pessoal@gmail.com>
2025-02-18 00:54:16 -0300

    i got duck feet

--- asynchronous/air_hostess/script.js
+++ asynchronous/air_hostess/script.js
@@ -3,3 +3,61 @@
  * separar cada ação em funções e chamar elas quando necessário
  * montar uma função principal só pra controlar o caminho das possibilidades
  */
+
+/**
+ * ------- INÍCIO -------
+ * Bom dia senhoras e senhores, bem vindos ao vôo 4321
+ * 
+ * Aqui quem fala é o seu capitão
+ * 
+ * Nós gentilmente pedimos que ponham seus cintos e que deixem as bandejas levantadas e bancos retos
+ * 
+ * (aparecer o botão do cinto. Colocar um efeito de pressionar no botão, além de desaparecer quando aperta)
+ * 
+ * A Gois Airlines agradece por escolher voar conosco, esperamos que tenham uma ótima viagem
+ * 
+ * ------- (NÃO APERTOU O CINTO) -------
+ * 
+ * (aeromoça aparece)
+ * Com licença, você ainda não apertou o seu cinto
+ * 
+ * caso a pessoa aperte fora do cinto, o botão do cinto salta
+ * 
+ * (apertou o cinto)
+ * Muito obrigada
+ * 
+ * ------- LANCHE -------
+ * Você aceita um lanche?
+ *  sim | não
+ * 
+ * (sim)
+    * Refrigerante ou café?
+    * Refrigente | café
+        * 
+        * (refrigerante)
+        * coca ou fanta?
+    * 
+    * biscoito doce ou salgado?
+    * doce | salgado
+    * 
+    * Aqui está!
+ * 
+ * (não)
+ * 
+ * ------- TURBULÊNCIA -------
+ * 
+ * Senhoras e senhores, nós estamos agora cruzando uma zona de turbulência
+ * 
+ * Por favor, retornem aos seus bancos e apertem seus cintos
+ * 
+ * ------- POUSO -------
+ * 
+ * Senhoras e senhores, aqui quem fala é o seu capitão
+ * 
+ * O avião pousará em instantes
+ * 
+ * O horário local é 8:23, estão fazendo 30º e o céu está limpo
+ * 
+ * Obrigado mais uma vez por voarem conosco, tenham um bom dia
+ */
+
